f6a495c8caa907d128c521fe7f314a28ab52400c
Gwynne <lobsgwynne37@gmail.com>
2025-05-17 15:18:17 +0800

    arranged files and added fonts

--- app/(client)/[id].tsx
+++ app/(client)/[id].tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function IndexPage() {
+export default function CapsuleIDPage() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-red-400">Hello, World!</Text>
