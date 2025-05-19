919e568a6bcb3a34cf669d569a9b91aef585dbf0
Gwynne <lobsgwynne37@gmail.com>
2025-05-20 00:55:33 +0800

    simple details on home page

--- app/(client)/home.tsx
+++ app/(client)/home.tsx
@@ -2,11 +2,9 @@ import { Text, View } from "react-native";
 
 export default function IndexPage() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      
-        <Text className="text-2xl font-montserrat">This is Inter Black</Text>
-        <Text className="text-2xl font-noto">This is Lexend Regular</Text>
-        <Text className="text-2xl font-noto-bold">This is Lexend Bold</Text>
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-white text-3xl font-montserrat-bold">choose a time {"\n"}capsule to view.</Text>
+        
       
     </View>
     
