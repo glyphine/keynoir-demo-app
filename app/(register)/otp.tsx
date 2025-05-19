148cc4ab1cf527797fa867373a9b82853950e8d0
Gwynne <lobsgwynne37@gmail.com>
2025-05-19 23:01:39 +0800

    finished otp.

---
+++ app/(register)/otp.tsx
@@ -0,0 +1,100 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { router } from "expo-router";
+import { useRef, useState } from "react";
+import { NativeSyntheticEvent, Text, TextInput, TextInputKeyPressEventData, TouchableOpacity, View } from "react-native";
+
+export default function OTPPage() {
+    const [otp, setOtp] = useState(["", "", "", "", "", ""]);
+  const inputs = useRef<(TextInput | null)[]>([]);
+
+  const handleChange = (value: string, index: number) => {
+    // Paste: handle full string paste
+    if (value.length > 1) {
+      const chars = value.slice(0, 6).split("");
+      setOtp(chars);
+      chars.forEach((char, i) => {
+        inputs.current[i]?.setNativeProps({ text: char });
+      });
+      inputs.current[5]?.focus();
+      return;
+    }
+
+    const newOtp = [...otp];
+    newOtp[index] = value;
+    setOtp(newOtp);
+
+    if (value && index < 5) {
+      inputs.current[index + 1]?.focus();
+    }
+  };
+
+  const handleKeyPress = (e: NativeSyntheticEvent<TextInputKeyPressEventData>, index: number) => {
+    if (e.nativeEvent.key === "Backspace" && otp[index] === "") {
+      const newOtp = [...otp];
+      newOtp[Math.max(index - 1, 0)] = "";
+      setOtp(newOtp);
+      inputs.current[Math.max(index - 1, 0)]?.focus();
+    }
+  };
+
+  const handleSubmit = () => {
+    const code = otp.join("");
+    console.log("OTP Submitted:", code);
+    router.push("/register");
+  };
+
+  return (
+    <View className="flex-1 bg-backgroundtwo px-12 pt-12 justify-between">
+      {/* Header + Title */}
+      <View>
+        <View className="flex-row items-center mb-6 mt-8">
+          <TouchableOpacity onPress={() => router.back()}>
+            <AntDesign name="left" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+
+        <Text className="text-3xl font-montserrat-bold text-black text-center mb-1">
+          otp.
+        </Text>
+        <Text className="text-center text-black text-base font-notosans-regular ">
+          Verify account with OTP.
+        </Text>
+      </View>
+
+      {/* OTP Inputs */}
+      <View className="flex-row justify-center items-center space-x-2 mt-8 mb-4">
+        {otp.map((digit, index) => (
+          <TextInput
+            key={index}
+            ref={(el: TextInput | null) => {
+              inputs.current[index] = el;
+            }}
+            value={digit}
+            onChangeText={(value) => handleChange(value, index)}
+            onKeyPress={(e) => handleKeyPress(e, index)}
+            maxLength={1}
+            keyboardType="number-pad"
+            className="w-10 h-14 text-center text-lg border border-black rounded-md bg-white mx-1"
+          />
+        ))}
+      </View>
+
+      {/* Footer: Resend + Submit */}
+      <View className="items-center mb-20">
+        <Text className="text-black font-notosans-regular text-base">Didn’t receive an OTP?</Text>
+        <TouchableOpacity onPress={() => console.log("Resend code")}>
+          <Text className="underline font-notosans-bold text-base text-black mt-4 mb-8">
+            Resend Code
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={handleSubmit}
+          className="bg-black py-4 rounded-2xl mt-8 w-full items-center"
+        >
+          <Text className="text-white font-bold text-base">SUBMIT</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
--- app/(register)/phone.tsx
+++ app/(register)/phone.tsx
@@ -1,11 +1,60 @@
-import { Text, View } from "react-native";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { router } from "expo-router";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function MobilePage() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-400">Hello, World!</Text>
-    </View>
+    <View className="flex-1 bg-black justify-center px-6">
+      {/* Logo */}
+      <View className="items-center mb-6">
+        <Image
+          source={require("@/assets/images/icon.png")}
+          resizeMode="contain"
+          className="w-56 h-32"
+        />
+      </View>
+
+      {/* Login Card */}
+      <View className="bg-backgroundtwo w-full rounded-xl p-6 space-y-4">
+        <Text className="text-3xl font-montserrat-bold text-black text-center mb-1">
+          enter.
+        </Text>
+        <Text className="text-center text-black text-sm font-notosans-regular mb-8">
+          Please input your mobile number.
+        </Text>
+
+        {/* Mobile Number */}
+        <TextInput
+          placeholder="Mobile No."
+          placeholderTextColor="#999"
+          className="border border-gray-300 bg-white text-black font-notosans-regular rounded-md px-4 py-3 mb-8"
+        />
+
+        {/* Country */}
+        <TouchableOpacity className="items-start">
+          <Text className="text-sm font-notosans-bold text-black mb-2">
+            Country: 
+          </Text>
+        </TouchableOpacity>
+
+        {/* Country number */}
+        <TextInput
+          placeholder="Philippines (+63)"
+          placeholderTextColor="#999"
+          secureTextEntry
+          className="border border-gray-300 bg-white text-black font-notosans-regular rounded-md px-4 py-3 mb-8"
+        />
+
     
-   
-  )
+
+        {/* Login Button */}
+        <TouchableOpacity
+          onPress={() => router.push("/otp")}
+          className="bg-black py-4 rounded-xl items-center mb-8"
+        >
+          <Text className="text-white text-lg font-montserrat-bold">NEXT <AntDesign name="right" size={16} color="white"/></Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
